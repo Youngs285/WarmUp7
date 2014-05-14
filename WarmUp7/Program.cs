8f93358c01e7b8be29b43e8759e2809e92d44ca1
Youngs285 <nitronite500@gmail.com>
2014-05-14 09:40:18 -0400

    unfinished

---
+++ WarmUp7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WarmUp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void IsPerfect(int n)
+        {
+
+        }
+    }
+}
